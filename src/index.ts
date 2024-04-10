3d1dcaba4a12c0b5703fafbbfc7d4685c35b0767
Tharshen2124 <tharshensurianbalan@gmail.com>
2024-04-10 22:10:17 +0800

    fix: header rows are now fixed

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,15 @@ import fs from 'fs';
 import PDFDocument from 'pdfkit'
 
 try {
-    // reads file content in utf8 encoding
-    const fileContent = fs.readFileSync('../test-folder/script.js', 'utf8')
-    console.log(fileContent)
+  // reads file content in utf8 encoding
+  const fileContent = fs.readFileSync('../test-folder/script.js', 'utf8')
 } catch (error: any) {
-    console.error(`Error reading file: ${error.message}`)
+  console.error(`Error reading file: ${error.message}`)
 }
 
 // Create a new PDF document object
+// A4 size is 595.28 x 841.89
 const doc = new PDFDocument({ size: 'A4' })
-console.log(doc)
 
 // create pdf document
 doc.pipe(fs.createWriteStream('output.pdf'))
@@ -21,26 +20,59 @@ const headers = ["Name", "Age"]
 
 // Define row data
 const rowData = [
-    ["Alice", "23"],
-    ["Bob", "30"],
-    ["Bob", "30"]
+  ["Alice", "23"],
+  ["Bob", "30"],
+  ["Bob", "30"]
 ]
 
-const columnHeight = 30
-let heightPoint = 20
+const headerTexts = [
+  { 
+    text: "Test", 
+    x: 25, 
+    y: 24 
+  },
+  { 
+    text: "Expected Result", 
+    x: 125, 
+    y: 24 
+  },
+  { 
+    text: "Actual Result", 
+    x: 255, 
+    y: 24 
+  },
+  { 
+    text: "Pass", 
+    x: 390, 
+    y: 24 
+  }
+]
 
-const columnWidth = 200;
-let widthPoint = 20
+const columnHeaderPoints = [
+  { x: 20, y: 20 },
+  { x: 170, y: 20 },
+  { x: 320, y: 20 },
+  { x: 470, y: 20 },
+  { x: 575, y: 20 }
+]
 
-doc.moveTo(20, 20)
-.lineTo(220, 20)
+const headerColumnHeight = 20
+const yStarterPoint = 20
 
-for(let i = 0; i<rowData.length; i++) {
-    doc.moveTo(220, heightPoint)
-    doc.lineTo(220, heightPoint + columnHeight)
-    .lineTo(20, heightPoint + columnHeight) 
-    .lineTo(20, heightPoint) 
-    heightPoint += columnHeight
+// create header row
+for(let i=0; i<columnHeaderPoints.length-1; i++) 
+{
+  doc.moveTo(columnHeaderPoints[i].x, columnHeaderPoints[i].y)
+    .lineTo(columnHeaderPoints[i+1].x, yStarterPoint)
+    .lineTo(columnHeaderPoints[i+1].x, yStarterPoint + headerColumnHeight)  
+    .lineTo(columnHeaderPoints[i].x, yStarterPoint + headerColumnHeight)
+    .lineTo(columnHeaderPoints[i].x, yStarterPoint)
+    .fillAndStroke("#d9d9d9", "#000")
+    .fontSize(12)
+    .fillColor('black')
+
+  const value = Math.round(doc.widthOfString(headerTexts[i].text))
+  doc.text(headerTexts[i].text, headerTexts[i].x, headerTexts[i].y)  
 }
     
 doc.stroke()
